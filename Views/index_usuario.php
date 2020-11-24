a1d61c52aed623f735f4ba3b45a25587e48ba53f
yruben <yruben98@gmail.com>
2020-11-24 21:42:58 +0100

    portada usuarios

---
+++ Views/index_usuario.php
@@ -0,0 +1,23 @@
+<?php
+
+class Index {
+
+    //Constructor
+    function __construct(){
+        $this->render();
+    }
+
+    //Muestra la cabecera, la pagina de inicio y el pie;
+    function render(){
+
+        include '../Views/Header.php';
+
+        include '../Views/Portada.php';
+
+        include '../Views/Footer.php';
+    }
+
+}
+
+
+?>
