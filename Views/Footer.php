bf81eb11278369f922c169b89465633b07d6e91c
yruben <yruben98@gmail.com>
2020-11-24 15:13:35 +0100

    Añadido Footer

--- Views/Footer.php
+++ Views/Footer.php
@@ -2,6 +2,16 @@
 
 <footer>
 
+    <nav id="barra-fija-inferior" class="nav">
+        <div class="container">
+            <div class="row" style="padding-top: 1%">
+                <a class="textoBlanco" href="https://www.uvigo.gal/">Aviso legal |</a>
+                <a class="textoBlanco" href="https://www.uvigo.gal/">Protección de datos |</a>
+                <a class="textoBlanco" href="https://www.uvigo.gal/">Accesibilidad</a>
+            </div>
+        </div>
+    </nav>
+
 </footer>
 
 </html>
