65a92fb9309ca4fa7fc3b2332209dc795906ed76
yruben <yruben98@gmail.com>
2020-12-01 22:22:46 +0100

    Index de usuario normal listo

--- Views/index_usuario.php
+++ Views/index_usuario.php
@@ -10,11 +10,11 @@ class Index {
     //Muestra la cabecera, la pagina de inicio y el pie;
     function render(){
 
-        include '../Views/Header.php';
+        include './Views/Header.php';
 
-        include '../Views/Portada.php';
+        include './Views/Portada.php';
 
-        include '../Views/Footer.php';
+        include './Views/Footer.php';
     }
 
 }
