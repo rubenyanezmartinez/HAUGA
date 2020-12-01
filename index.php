844e70268b8f2f5f2ec688c804e984ef7896f581
yruben <yruben98@gmail.com>
2020-12-01 22:22:35 +0100

    Index.php actualizado, ahora ya redirige a la página principal

--- index.php
+++ index.php
@@ -1,29 +1,8 @@
 <?php
-//entrada a la aplicacion
 
-//session
-echo('index');
 session_start();
-//incluir funcion autenticacion
 
-include './Functions/Authentication.php';
-//si no esta autenticado
-if (!IsAuthenticated()){
-    header('Location: ./Controllers/User_Controller.php');
-} else{   //esta autenticado
-    include './Models/USUARIO_Model.php';
-    $usuario = new USUARIO_Model(null, $_SESSION['login'],'','','','','','',
-        '','','','', '','','','');
-    $usuario->RellenaDatos();
-
-    if($usuario->rol == 'ADMIN'){
-        include './Views/index_admin.php';
-        new AdminIndex();
-    } else{
-        include './Views/index_usuario.php';
-        new Index();
-    }
-
-}
+include './Views/index_usuario.php';
+new Index();
 
 ?>
