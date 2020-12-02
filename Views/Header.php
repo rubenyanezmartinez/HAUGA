0841bfde69bad07e1a903915abc78f2e602de791
yruben <yruben98@gmail.com>
2020-12-02 15:01:12 +0100

    Enlazada menu cabecera para usuarios SHOWALL

--- Views/Header.php
+++ Views/Header.php
@@ -98,7 +98,7 @@ include_once '../Functions/esAdministrador.php';
                         <h5><a href="#" class="textoAzul">GRUPOS INVESTIGACIÓN</a></h5>
                     </div>
                     <div class="col-2 align-self-center text-center">
-                        <h5><a href="../Controllers/User_Controller.php" class="textoAzul">USUARIOS</a></h5>
+                        <h5><a href="../Controllers/User_Controller.php?action=showall" class="textoAzul">USUARIOS</a></h5>
                     </div>
                 <?php } else { ?>
                     <div class="col-2 align-self-center text-center">
