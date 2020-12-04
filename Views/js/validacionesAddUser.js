38b5dd6b3cc81de15dab447ffb81db56d95ec137
yruben <yruben98@gmail.com>
2020-12-04 17:37:56 +0100

    Corregidas validaciones

--- Views/js/validacionesAddUser.js
+++ Views/js/validacionesAddUser.js
@@ -40,151 +40,124 @@ $(document).ready(function() {
 
     $("#addUserForm").validate({
         rules: {
-            nombre: {
-                maxLength: 15,
-                minLength: 1,
-                required: true,
-                lettersonly: true
+            "nombre": {
+                "required": true,
+                "lettersonly": true
             },
-            apellidos: {
-                maxLength: 25,
-                minLength: 1,
-                required: true,
-                lettersonly: true
+            "apellidos": {
+                "required": true,
+                "lettersonly": true
             },
-            password: {
-                nowhitespace: true,
-                alphanumeric: true,
-                maxLength: 64,
-                minLength: 1,
-                required: true
+            "password": {
+                "nowhitespace": true,
+                "alphanumeric": true,
+                "required": true
             },
-            fecha_nacimiento: {
-                nowhitespace: true,
-                required: true
+            "fecha_nacimiento": {
+                "nowhitespace": true,
+                "required": true
             },
-            email_usuario: {
-                nowhitespace: true,
-                email: true,
-                maxLength: 30,
-                minLength: 1,
-                required: true
+            "email_usuario": {
+                "nowhitespace": true,
+                "email": true,
+                "required": true
             },
-            telef_usuario: {
-                nowhitespace: true,
-                digit: true,
-                maxLength: 9,
-                minLength: 1,
-                required: true
+            "telef_usuario": {
+                "nowhitespace": true,
+                "digit": true,
+                "required": true
             },
-            dni: {
-                nowhitespace: true,
-                maxLength: 9,
-                minLength: 9,
-                required: true,
-                dniCheck: true
+            "dni": {
+                "nowhitespace": true,
+                "required": true,
+                "dniCheck": true
             },
-            rol: {
-                required: true
+            "rol": {
+                "required": true
             },
-            afiliacion: {
-                required: true
+            "afiliacion": {
+                "required": true
             },
-            nombre_puesto: {
-                nowhitespace: true,
-                maxLength: 60,
-                minLength: 1,
+            "nombre_puesto": {
+                "nowhitespace": true
             },
-            nivel_jerarquia: {
-                nowhitespace: true,
-                digit: true
+            "nivel_jerarquia": {
+                "nowhitespace": true,
+                "digit": true
             },
-            depart_usuario: {
-                nowhitespace: true,
-                digit: true
+            "depart_usuario": {
+                "nowhitespace": true,
+                "digit": true
             },
-            grupo_usuario: {
-                nowhitespace: true,
-                digit: true
+            "grupo_usuario": {
+                "nowhitespace": true,
+                "digit": true
             },
-            centro_usuario: {
-                nowhitespace: true,
-                digit: true
+            "centro_usuario": {
+                "nowhitespace": true,
+                "digit": true
             }
         },
         messages : {
-            nombre: {
-                required: "El campo no puede estar vacío",
-                minLength: "El campo no puede estar vacío.",
-                maxLength: "El campo no puede exceder los 15 caracteres.",
-                lettersonly: "El campo debe ser alfabético"
+            "nombre": {
+                "required": "El campo no puede estar vacío",
+                "lettersonly": "El campo debe ser alfabético"
             },
-            apellidos: {
-                required: "El campo no puede estar vacío",
-                minLength: "El campo no puede estar vacío.",
-                maxLength: "El campo no puede exceder los 25 caracteres.",
-                lettersonly: "El campo debe ser alfabético"
+            "apellidos": {
+                "required": "El campo no puede estar vacío",
+                "lettersonly": "El campo debe ser alfabético"
             },
-            password: {
-                required: "El campo no puede estar vacío",
-                minLength: "El campo no puede estar vacío.",
-                maxLength: "El campo no puede exceder los 64 caracteres.",
-                alphanumeric: "El campo debe ser alfanumerico.",
-                nowhitespace: "El campo no puede ser un espacio en blanco"
+            "password": {
+                "required": "El campo no puede estar vacío",
+                "alphanumeric": "El campo debe ser alfanumerico.",
+                "nowhitespace": "El campo no puede ser un espacio en blanco"
             },
-            fecha_nacimiento: {
-                required: "El campo no puede estar vacío",
-                nowhitespace: "El campo no puede ser un espacio en blanco"
+            "fecha_nacimiento": {
+                "required": "El campo no puede estar vacío",
+                "nowhitespace": "El campo no puede ser un espacio en blanco"
             },
-            email_usuario: {
-                required: "El campo no puede estar vacío",
-                minLength: "El campo no puede estar vacío.",
-                maxLength: "El campo no puede exceder los 30 caracteres.",
-                email: "El campo debe ser un email.",
-                nowhitespace: "El campo no puede ser un espacio en blanco"
+            "email_usuario": {
+                "required": "El campo no puede estar vacío",
+                "email": "El campo debe ser un email.",
+                "nowhitespace": "El campo no puede ser un espacio en blanco"
             },
-            telef_usuario: {
-                required: "El campo no puede estar vacío",
-                minLength: "El campo no puede estar vacío.",
-                maxLength: "El campo no puede exceder los 9 caracteres.",
-                digit: "El campo debe estar formado por números.",
-                nowhitespace: "El campo no puede ser un espacio en blanco"
+            "telef_usuario": {
+                "required": "El campo no puede estar vacío",
+                "digit": "El campo debe estar formado por números.",
+                "nowhitespace": "El campo no puede ser un espacio en blanco"
             },
-            dni: {
-                required: "El campo no puede estar vacío",
-                minLength: "El campo no debe tener 9 caracteres.",
-                maxLength: "El campo no puede exceder los 9 caracteres.",
-                dniCheck: "El campo debe ser un dni correcto.",
-                nowhitespace: "El campo no puede ser un espacio en blanco"
+            "dni": {
+                "required": "El campo no puede estar vacío",
+                "dniCheck": "El campo debe ser un dni correcto.",
+                "nowhitespace": "El campo no puede ser un espacio en blanco"
             },
-            rol: {
-                required: "El campo no puede estar vacío"
+            "rol": {
+                "required": "El campo no puede estar vacío"
             },
-            afiliacion: {
-                required: "El campo no puede estar vacío"
+            "afiliacion": {
+                "required": "El campo no puede estar vacío"
             },
-            nombre_puesto: {
-                minLength: "El campo no debe estar vacío.",
-                maxLength: "El campo no puede exceder los 60 caracteres.",
-                nowhitespace: "El campo no puede ser un espacio en blanco"
+            "nombre_puesto": {
+                "nowhitespace": "El campo no puede ser un espacio en blanco"
             },
-            nivel_jerarquia: {
-                nowhitespace: "El campo no puede ser un espacio en blanco",
-                digit: "El campo debe estar formado por números."
+            "nivel_jerarquia": {
+                "nowhitespace": "El campo no puede ser un espacio en blanco",
+                "digit": "El campo debe estar formado por números."
             },
-            depart_usuario: {
-                nowhitespace: "El campo no puede ser un espacio en blanco",
-                digit: "El campo debe estar formado por números."
+            "depart_usuario": {
+                "nowhitespace": "El campo no puede ser un espacio en blanco",
+                "digit": "El campo debe estar formado por números."
             },
-            grupo_usuario: {
-                nowhitespace: "El campo no puede ser un espacio en blanco",
-                digit: "El campo debe estar formado por números."
+            "grupo_usuario": {
+                "nowhitespace": "El campo no puede ser un espacio en blanco",
+                "digit": "El campo debe estar formado por números."
             },
-            centro_usuario: {
-                nowhitespace: "El campo no puede ser un espacio en blanco",
-                digit: "El campo debe estar formado por números."
+            "centro_usuario": {
+                "nowhitespace": "El campo no puede ser un espacio en blanco",
+                "digit": "El campo debe estar formado por números."
             }
         },
+        errorElement: "label"
 
     });
 
@@ -200,10 +173,6 @@ $(document).ready(function() {
         }
         return this.optional(element);
     }, "DNI no válido");
-    $("#botonAddUser").click(function(){
-        $("#addUserForm").submit();
-        return false;
-    });
 
     $("#botonAtras").click(function(){
         window.location.href = "../Controllers/User_Controller.php?action=showall";
