a8a03e1a23369da05be65688dfb5fef4f05900f4
yruben <yruben98@gmail.com>
2020-12-04 17:26:47 +0100

    Solucionado problema con las validaciones dle login

--- Views/js/validacionesLogin.js
+++ Views/js/validacionesLogin.js
@@ -1,42 +1,31 @@
 $(document).ready(function() {
+
     $("#loginForm").validate({
         rules: {
-            login: {
-                nowhitespace: true,
-                alphanumeric: true,
-                maxLength: 12,
-                minLength: 1,
-                required: true
+            "login": {
+                "required": true,
+                "nowhitespace": true,
+                "alphanumeric": true
             },
-            password: {
-                nowhitespace: true,
-                alphanumeric: true,
-                maxLength: 64,
-                minLength: 1,
-                required: true
+            "password": {
+                "required": true,
+                "nowhitespace": true,
+                "alphanumeric": true
+
             }
         },
         messages : {
-            login: {
-                required: "El campo no puede estar vacío",
-                minLength: "El campo no puede estar vacío.",
-                maxLength: "El campo no puede exceder los 12 caracteres.",
-                alphanumeric: "El campo debe ser alfanumerico.",
-                nowhitespace: "El campo no puede ser un espacio en blanco"
+            "login": {
+                "required": "El campo no puede estar vacío",
+                "alphanumeric": "El campo debe ser alfanumerico.",
+                "nowhitespace": "El campo no puede ser un espacio en blanco"
             },
-            password: {
-                required: "El campo no puede estar vacío",
-                minLength: "El campo no puede estar vacío.",
-                maxLength: "El campo no puede exceder los 64 caracteres.",
-                alphanumeric: "El campo debe ser alfanumerico.",
-                nowhitespace: "El campo no puede ser un espacio en blanco"
+            "password": {
+                "required": "El campo no puede estar vacío",
+                "alphanumeric": "El campo debe ser alfanumerico.",
+                "nowhitespace": "El campo no puede ser un espacio en blanco"
             }
         },
-
-    });
-
-    $("#botonLogin").click(function(){
-        $("#loginForm").submit();
-        return false;
+        errorElement: "label"
     });
 });
