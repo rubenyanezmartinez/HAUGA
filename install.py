c089b32f2a79caba4371dc2f43fadcac70627a93
yruben <yruben98@gmail.com>
2020-12-15 22:06:15 +0100

    Mejorados mensajes de install.py

--- install.py
+++ install.py
@@ -5,6 +5,14 @@ class colores_indicadores:
     COLOR_ROJO = '\033[91m'
     COLOR_POR_DEFECTO = '\033[0m'
 
+def imprimir_mensaje(mensaje, codigo_color):
+    if codigo_color == 1:
+        print(colores_indicadores.COLOR_VERDE + mensaje + colores_indicadores.COLOR_POR_DEFECTO)
+    else:
+       print(colores_indicadores.COLOR_ROJO + mensaje + colores_indicadores.COLOR_POR_DEFECTO)
+
+
+
 os.system('rm -r ./HAUGA/')
 os.system('rm -r /var/www/html/HAUGA/')
 
@@ -13,28 +21,20 @@ gitclone = os.system('git clone https://github.com/rubenyanezmartinez/HAUGA.git;
 if gitclone == 0:
     os.system('mv ./HAUGA /var/www/html/;')
     os.system('cd /var/www/html/HAUGA/ || exit;')
-    print(colores_indicadores.COLOR_VERDE)
-    print('Se ha descargado y colocado con exito el proyecto')
+    imprimir_mensaje('Se ha descargado y colocado con exito el proyecto', 1)
 else:
-    print(colores_indicadores.COLOR_ROJO)
-    print('ERROR: no ha sido posible descargar el proyecto')
+    imprimir_mensaje('ERROR: no ha sido posible descargar el proyecto', 0)
 
 
 permisoImagenes = os.system('chmod -R a+w /var/www/html/HAUGA/Models/Imagenes_Espacios/;')
 if permisoImagenes == 0:
-    print(colores_indicadores.COLOR_VERDE)
-    print('Se han concedido con exito permisos de escritura al directorio Imagenes_Espacios')
+    imprimir_mensaje('Se han concedido con exito permisos de escritura al directorio Imagenes_Espacios',1)
 else:
-    print(colores_indicadores.COLOR_ROJO)
-    print('ERROR: no ha sido posible conceder permisos de escritura al directorio Imagenes_Espacios')
+    imprimir_mensaje('ERROR: no ha sido posible conceder permisos de escritura al directorio Imagenes_Espacios',0)
 
 
 montarBaseDeDatos = os.system('mysql -u root --password=iu < /var/www/html/HAUGA/Models/db.sql;')
 if montarBaseDeDatos == 0:
-    print(colores_indicadores.COLOR_VERDE)
-    print('Se ha montado correctamente la BD')
+    imprimir_mensaje('Se ha montado correctamente la BD',1)
 else:
-    print(colores_indicadores.COLOR_ROJO)
-    print('ERROR: no se ha podido montar correctamente la BD')
-
-print(colores_indicadores.COLOR_POR_DEFECTO)
+    imprimir_mensaje('ERROR: no se ha podido montar correctamente la BD',0)
