0589800e1bd6ce6442a42956fd00c0f6e1bb642f
Inés Prieto <ipgonzalez2@esei.uvigo.es>
2021-01-17 17:23:44 +0100

    SHOWALL CORREGIDO ERROR CENTRO

--- Views/CENTRO_SHOWALL_View.php
+++ Views/CENTRO_SHOWALL_View.php
@@ -71,13 +71,6 @@ class CENTRO_SHOWALL_View{
                             <th><?=$this->nombreEdificios[$centro->getCentroId()]?></th>
                             <th style="text-align: center">
 
-                                <a href="../Controllers/Centro_Controller.php?action=showcurrent&centro_id=<?=$centro->getCentroId()?>">
-                                    <svg style="color: green" width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
-                                        <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
-                                    </svg>
-                                </a>
-
                                 <?php if(IsAuthenticated() && esAdministrador()) {?>
                                     <a class="eliminar" data-enlace="<?=$centro->getCentroId()?>">
                                         <svg style="color: #ff0000" width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
