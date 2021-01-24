0befadaad3cb6e80b83d044c0358be7ca7a2e97c
yruben <yruben98@gmail.com>
2021-01-24 15:02:34 +0100

    Cambiada estética botón buscar espacio

--- Views/ESPACIO_SHOWALL_View.php
+++ Views/ESPACIO_SHOWALL_View.php
@@ -320,7 +320,12 @@ class ESPACIO_SHOWALL_View
                     <h2 class="text-center textoAzul">Espacios</h2>
                 </div>
                 <div class="col text-right">
-
+                    <a class="btn btn-primary" style="background-color: #073349;" href="../Controllers/Espacio_Controller.php?action=search">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                        </svg>
+                        Buscar Espacios
+                    </a>
                 </div>
             </div>
             <hr>
@@ -376,13 +381,6 @@ class ESPACIO_SHOWALL_View
                     </tbody>
                 </table>
             </div>
-<div>  <a class="btn btn-primary" style="background-color: #073349;" href="../Controllers/Espacio_Controller.php?action=search">
-        <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-            <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-        </svg>
-        Buscar Espacios
-    </a></div>
             <div class="row">
                 <div class="col">
                     <nav aria-label="paginacion">
