1b02a31c2b87c6601eba647fad14a99466a550e7
Inés Prieto <ipgonzalez2@esei.uvigo.es>
2021-01-24 12:27:09 +0100

    Iconos incidencias

--- Views/INCIDENCIA_ADD_View.php
+++ Views/INCIDENCIA_ADD_View.php
@@ -127,17 +127,28 @@ class INCIDENCIA_ADD_View{
 
                         <div class ="row">
                             <div class="col-md-6  mb-2" style="margin-bottom: 1rem!important;">
-                                <button id="botonAddIncidencia" type='submit' name='action' value='addIncidencia' class="btn btn-success">
+                                <button id="botonAddIncidencia" type='submit' name='action' value='addIncidencia' class="btn btn-success" style="margin-right: 5px;">
                                     Crear Incidencia
                                     <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-plus-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                         <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
                                     </svg>
                                 </button>
+
+
                             </div>
                         </div>
 
                     </form>
+                    <button class="btn btn-info">
+
+                        <a id="botonShowIncidencia" href="../Controllers/Incidencia_Controller.php?action=showall">
+                            Ver incidencias
+                            <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                            </svg>
+                        </a>
+                    </button>
                 </div>
 
             </div>
--- Views/INCIDENCIA_SHOWALL_View.php
+++ Views/INCIDENCIA_SHOWALL_View.php
@@ -51,16 +51,14 @@ class INCIDENCIA_SHOWALL_View{
                             <th><?=$incidencia->getEstadoIncid()?></th>
                             <th style="text-align: center">
                                 <a href="../Controllers/Incidencia_Controller.php?action=aceptar&incidencia_id=<?=$incidencia->getIncidenciaId()?>">
-                                    <svg style="color: green" width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
-                                        <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
+                                        <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
                                     </svg>
                                 </a>
 
                                 <a href="../Controllers/Incidencia_Controller.php?action=denegar&incidencia_id=<?=$incidencia->getIncidenciaId()?>">
-                                    <svg style="color: red" width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-search" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                        <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
-                                        <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+                                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                                     </svg>
                                 </a>
                                 
