9edf6e1cc0f70b39c76e51efd975344565cd4f19
Inés Prieto <ipgonzalez2@esei.uvigo.es>
2021-01-24 12:31:26 +0100

    Botones incidencia

--- Views/INCIDENCIA_SHOWALL_View.php
+++ Views/INCIDENCIA_SHOWALL_View.php
@@ -50,6 +50,9 @@ class INCIDENCIA_SHOWALL_View{
                             <th><?=$incidencia->getDescripcionIncid()?></th>
                             <th><?=$incidencia->getEstadoIncid()?></th>
                             <th style="text-align: center">
+                        <?php
+                        if($incidencia->getEstadoIncid() == "PEND"){?>
+
                                 <a href="../Controllers/Incidencia_Controller.php?action=aceptar&incidencia_id=<?=$incidencia->getIncidenciaId()?>">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
                                         <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
@@ -62,6 +65,8 @@ class INCIDENCIA_SHOWALL_View{
                                     </svg>
                                 </a>
                                 
+
+                            <?php } ?>
                             </th>
                         </tr>
                         <?php
